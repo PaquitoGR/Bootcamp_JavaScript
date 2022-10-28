a9df63d2572f74d1d5d5e5248a697b911b2bd945
PaquitoGR <paco.max@gmail.com>
2022-10-28 15:15:04 +0200

    Entrega

--- Modulo-02_Laboratorio_Variables_y_Tipos_de_datos/js/app copy.js
+++
@@ -1,5 +0,0 @@
-var review = prompt("Introduzca su reseña (del 1 al 5)")
-var anonymous = confirm("¿quiére que su reseña sea anónima)")
-
-document.getElementById("rating").innerHTML = review + " estrellas"  
-document.getElementById("anonymous").checked = anonymous
