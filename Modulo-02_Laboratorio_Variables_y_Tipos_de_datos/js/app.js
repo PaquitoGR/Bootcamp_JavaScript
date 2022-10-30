202d4d396b229aeb8d7281576ef9a708dfc33497
PaquitoGR <paco.max@gmail.com>
2022-10-30 14:43:48 +0100

    entrega

--- Modulo-02_Laboratorio_Variables_y_Tipos_de_datos/js/app.js
+++ Modulo-02_Laboratorio_Variables_y_Tipos_de_datos/js/app.js
@@ -20,17 +20,36 @@ var hotel = {
 };
 
 var hotelSel = prompt("Introduzca el nombre del hotel");
-var review = prompt("Introduzca su reseña (del 1 al 5)");
+var ratingSel = prompt("Introduzca su reseña (del 1 al 5)");
+//var ratingSel = prompt("Introduzca su reseña (una, dos, tres, cuatro o cinco)");
 var anonymous = confirm("¿quiére que su reseña sea anónima)");
 
 document.getElementById("name-hotel").innerHTML = "Hotel " + hotel[hotelSel].name;
 document.getElementById("location-hotel").innerHTML = "Ubicación: " + hotel[hotelSel].location;
 document.getElementById("img-hotel").src = hotel[hotelSel].image;
 
-// estrellas vacías
-var noStar = 5 - review;
+// Solución propia para las estrellas: El usuario introduce la puntuación en número y se calculan las estrellas vacías,
+// con las que se completará la linea de estrellas, restando a 5 el número de estrellas introducias por el usuario:
+var noStar = 5 - ratingSel;
 
-// estrellas mostradas = estrellas de la review, completando con estrellas vacías hasta 5
-document.getElementById("rating").innerHTML = "<span class='orangeStar'>&#9733</span>".repeat(review) + "<span class='orangeStar'>&#9734</span>".repeat(noStar);
+// Estrellas mostradas = estrellas de la ratingSel + estrellas vacías, usando el método de string "repeat"
+// que devuelve la cadena repetida las veces que se indiquen entre paréntesis.
+document.getElementById("rating").innerHTML = "<span class='orangeStar'>&#9733</span>".repeat(ratingSel) + "<span class='blackStar'>&#9734</span>".repeat(noStar);
 
+
+
+/*
+// Solución propuesta en clase para las estrellas: El usuario introduce a puntuación en formato string, un objeto que almacena las 5 posibilidades
+// y muestra el número de estrellas seleccionadas:
+
+var stars = {
+    una : "<span class='orangeStar'>&#9733</span><span class='blackStar'>&#9734</span><span class='blackStar'>&#9734</span><span class='blackStar'>&#9734</span><span class='blackStar'>&#9734</span>",
+    dos : "<span class='orangeStar'>&#9733</span><span class='orangeStar'>&#9733</span><span class='blackStar'>&#9734</span><span class='blackStar'>&#9734</span><span class='blackStar'>&#9734</span>",
+    tres : "<span class='orangeStar'>&#9733</span><span class='orangeStar'>&#9733</span><span class='orangeStar'>&#9733</span><span class='blackStar'>&#9734</span><span class='blackStar'>&#9734</span>",
+    cuatro : "<span class='orangeStar'>&#9733</span><span class='orangeStar'>&#9733</span><span class='orangeStar'>&#9733</span><span class='orangeStar'>&#9733</span><span class='blackStar'>&#9734</span>",
+    cinco : "<span class='orangeStar'>&#9733</span><span class='orangeStar'>&#9733</span><span class='orangeStar'>&#9733</span><span class='orangeStar'>&#9733</span><span class='orangeStar'>&#9733</span>",
+};
+
+document.getElementById("rating").innerHTML = stars[ratingSel];
 document.getElementById("anonymous").checked = anonymous;
+*/
