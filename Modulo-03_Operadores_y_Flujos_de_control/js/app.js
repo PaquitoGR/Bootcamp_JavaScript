13b91bd7a981696269e5f76eb8711e1d7df48b24
PaquitoGR <paco.max@gmail.com>
2022-11-02 22:17:47 +0100

    editando IRPF

--- Modulo-03_Operadores_y_Flujos_de_control/js/app.js
+++ Modulo-03_Operadores_y_Flujos_de_control/js/app.js
@@ -1,4 +1,4 @@
-
+/*
 const product = { 
     count: 3,
     price: 12.55,
@@ -6,7 +6,7 @@ const product = {
 };
 
 // Calculamos los diferentes valores de IVA segun el tipo de producto
-let iva = 0.21;       // valor por defecto
+var iva = 0.21;       // valor por defecto
 
 // Modo IF
 // if (product.type === "alimentacion") iva = 0.1;
@@ -23,13 +23,15 @@ switch (product.type) {
 };
 
 // Comprobamos que el campo COUNT no es cero ni negativo, en cuyo caso el total queda en 0
-let total = product.count > 0 ? product.count * product.price : 0;
+var total = product.count > 0 ? product.count * product.price : 0;
 
-let totalIva = total + total * iva;
+var totalIva = total + total * iva;
 
 console.log("El total con IVA incluido, es", totalIva.toFixed(2) + "€");
 
 console.log("**************************");
+
+*/
 
 // EXTRA 1: Calcular sueldo neto en nómina
 
@@ -38,29 +40,59 @@ const empleado = {
     hijos: 2,
     pagas: 14
 };
+/*
+30000 - 20200 = 9800
+20200 - 12450 = 7750
+
+(12450 - 12450*0.19) + (7750 - 7750*0.24) + (9800 - 9800*0.30)
+
+*/
+
+console.log("La cuenta es: ", (12450 - 12450*0.19) + (7750 - 7750*0.24) + (9800 - 9800*0.30))
 
 
-let retencion = 0;
 if (empleado.bruto < 12450) {
-    retencion = 0.19;
+    var netoAnual = empleado.bruto - empleado.bruto * 0.19;
+
 } else if (empleado.bruto < 20200) {
-        retencion = 0.24;
+    
+        var tramo1 = empleado.bruto - 12450;
+        netoAnual = ( 12450 - 12450 * 0.19 ) + (tramo1 - tramo1 * 0.24);
+
     } else if (empleado.bruto < 35.200) {
-            retencion = 0.30;
+            var tramo1 = empleado.bruto - 12450;
+            var tramo2 = empleado.bruto - 20200;
+            var netoAnual = ( 12450 - 12450 * 0.19 ) + (tramo1 - tramo1 * 0.24) + (tramo2 - tramo2 * 0.30);
+
         } else if (empleado.bruto < 60000) {
-                retencion = 0.37;
+                var tramo1 = empleado.bruto - 12450;
+                var tramo2 = empleado.bruto - 20200;
+                var tramo3 = empleado.bruto - 35200;
+                var netoAnual = ( 12450 - 12450 * 0.19 ) + (tramo1 - tramo1 * 0.24) + (tramo2 - tramo2 * 0.30) + (tramo3 - tramo3 * 0.37);       
+
             } else if (empleado.bruto < 300000) {
-                    retencion = 0.45;
+                    var tramo1 = empleado.bruto - 12450;
+                    var tramo2 = empleado.bruto - 20200;
+                    var tramo3 = empleado.bruto - 35200;
+                    var tramo4 = empleado.bruto - 60000;
+                    var netoAnual = ( 12450 - 12450 * 0.19 ) + (tramo1 - tramo1 * 0.24) + (tramo2 - tramo2 * 0.30) + (tramo3 - tramo3 * 0.37) + (tramo4 - tramo4 * 0.45);
+
                 } else {
-                    retencion = 0.47;
+                    var tramo1 = empleado.bruto - 12450;
+                    var tramo2 = empleado.bruto - 20200;
+                    var tramo3 = empleado.bruto - 35200;
+                    var tramo4 = empleado.bruto - 60000;
+                    var tramo5 = empleado.bruto - 300000;
+                    var netoAnual = ( 12450 - 12450 * 0.19 ) + (tramo1 - tramo1 * 0.24) + (tramo2 - tramo2 * 0.30) + (tramo3 - tramo3 * 0.37) + (tramo4 - tramo4 * 0.45) + (tramo5 - tramo5 * 0.47);
                 }
 
+               
 // Calculamos la retención con rebaja en caso de hijos
-retencion = empleado.hijos > 0 ? retencion -= 0.02 : retencion; 
+// retencion = empleado.hijos > 0 ? retencion -= 0.02 : retencion; 
 
 // Cálculo de la retribución neta anual y mensual
-let netoAnual = empleado.bruto - empleado.bruto * retencion;
-let netoMes = netoAnual/empleado.pagas;
+// var netoAnual = empleado.bruto - empleado.bruto * retencion;
+var netoMes = netoAnual/empleado.pagas;
 
 console.log("El total anual del empleado es", netoAnual + "€");
 console.log("El neto mensual es", netoMes.toFixed(2) + "€");
